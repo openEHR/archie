aeac8b7d8141d9312447946559ea17a456df2d7a
Pieter Bos <pieter.bos@nedap.com>
2020-07-16 16:43:32 +0200

    Refactor ConversionTest

--- bmm/src/test/java/org/openehr/bmm/v2/persistence/converters/ConversionTest.java
+++ bmm/src/test/java/org/openehr/bmm/v2/persistence/converters/ConversionTest.java
@@ -1,7 +1,9 @@
 package org.openehr.bmm.v2.persistence.converters;
 
 import org.junit.Test;
-import org.openehr.bmm.core.*;
+import org.openehr.bmm.core.BmmClass;
+import org.openehr.bmm.core.BmmGenericType;
+import org.openehr.bmm.core.BmmModel;
 import org.openehr.bmm.v2.persistence.PBmmSchema;
 import org.openehr.bmm.v2.persistence.odin.BmmOdinParser;
 import org.openehr.bmm.v2.persistence.odin.BmmOdinSerializer;
@@ -9,12 +11,13 @@ import org.openehr.bmm.v2.validation.BmmRepository;
 import org.openehr.bmm.v2.validation.BmmSchemaConverter;
 import org.openehr.bmm.v2.validation.BmmValidationResult;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.openehr.bmm.v2.persistence.converters.BmmTestUtil.parse;
 
 public class ConversionTest {
 
@@ -31,14 +34,14 @@ public class ConversionTest {
             System.out.println(validationResult.getLogger());
             assertTrue("the OpenEHR ADL Test 1.0.0 BMM files should pass validation", validationResult.passes());
         }
-        AdlTestModel = repo.getModel("openehr_adltest_1.0.0").getModel();
+        BmmModel adlTestModel = repo.getModel("openehr_adltest_1.0.0").getModel();
 
         // Descendant relations
         assertTrue ("\"GENERIC_CHILD_CLOSED\" descendant of \"GENERIC_PARENT<SUPPLIER_A,SUPPLIER_B>\"",
-                AdlTestModel.descendantOf("GENERIC_CHILD_CLOSED", "GENERIC_PARENT<SUPPLIER_A,SUPPLIER_B>"));
+                adlTestModel.descendantOf("GENERIC_CHILD_CLOSED", "GENERIC_PARENT<SUPPLIER_A,SUPPLIER_B>"));
 
         // Ancestor relations
-        List<String> testResult = AdlTestModel.getAllAncestorClasses("GENERIC_CHILD_OPEN_T");
+        List<String> testResult = adlTestModel.getAllAncestorClasses("GENERIC_CHILD_OPEN_T");
         // conformance result from ADL Workbench
         List<String> conformanceResult = new ArrayList<>(Arrays.asList("GENERIC_PARENT<T,SUPPLIER_B>"));
 
@@ -46,118 +49,6 @@ public class ConversionTest {
 
     }
 
-    @Test
-    public void testRm102() throws Exception {
-        BmmRepository repo = new BmmRepository();
-        repo.addPersistentSchema(parse("/openehr/openehr_basic_types_102.bmm"));
-        repo.addPersistentSchema(parse("/openehr/openehr_demographic_102.bmm"));
-        repo.addPersistentSchema(parse("/openehr/openehr_ehr_102.bmm"));
-        repo.addPersistentSchema(parse("/openehr/openehr_primitive_types_102.bmm"));
-        repo.addPersistentSchema(parse("/openehr/openehr_rm_102.bmm"));
-        repo.addPersistentSchema(parse("/openehr/openehr_structures_102.bmm"));
-
-        BmmSchemaConverter converter = new BmmSchemaConverter(repo);
-        converter.validateAndConvertRepository();
-        for (BmmValidationResult validationResult:repo.getModels()) {
-            System.out.println(validationResult.getLogger());
-            assertTrue("the OpenEHR RM 1.0.2 BMM files should pass validation", validationResult.passes());
-        }
-
-        Rm102Model = repo.getModel("openehr_ehr_1.0.2").getModel();
-        testRm102ModelPaths ();
-        testRm102PropertyMetatypes ();
-        testRm102ModelDescendants();
-        testRm102CompositionImmediateSuppliers();
-        testRm102PartyIdentifiedSuppliers();
-    }
-
-    public void testRm102ModelPaths() throws Exception {
-        // check some paths through the model
-        assertTrue ("property defined in class: (\"CARE_ENTRY\", \"/protocol\")", Rm102Model.hasPropertyAtPath("CARE_ENTRY", "/protocol"));
-        assertTrue ("property defined in descendant: (\"CARE_ENTRY\", \"/data/events/data\")", Rm102Model.hasPropertyAtPath("CARE_ENTRY", "/data/events/data"));
-        assertTrue ("property defined in ancestor: (\"OBSERVATION\", \"/protocol\")", Rm102Model.hasPropertyAtPath("OBSERVATION", "/protocol"));
-        assertTrue ("property defined in class: (\"OBSERVATION\", \"/data/events/data\")", Rm102Model.hasPropertyAtPath("OBSERVATION", "/data/events/data"));
-        assertTrue ("property defined in descendant: (\"OBSERVATION\", \"/data/events/data/items\")", Rm102Model.hasPropertyAtPath("OBSERVATION", "/data/events/data/items"));
-        assertFalse ("non-existent property: (\"OBSERVATION\", \"/data/events/data/xxx\")", Rm102Model.hasPropertyAtPath("OBSERVATION", "/data/events/data/xxx"));
-        assertTrue ("property defined in class: (\"COMPOSITION\", \"/context\")", Rm102Model.hasPropertyAtPath("COMPOSITION", "/context"));
-        assertTrue ("property defined in descendant: (\"CLUSTER\", \"/items/items/items\")", Rm102Model.hasPropertyAtPath("CLUSTER", "/items/items/items"));
-        assertTrue ("property defined in class: (\"DV_QUANTITY\", \"/normal_range\")", Rm102Model.hasPropertyAtPath("DV_QUANTITY", "/normal_range"));
-    }
-
-    public void testRm102PropertyMetatypes() throws Exception {
-        // check meta-types of some properties at paths
-        assertTrue ("BmmSimpleType at path: (\"OBSERVATION\", \"/protocol\")", Rm102Model.propertyAtPath("OBSERVATION", "/protocol").getType() instanceof BmmSimpleType);
-        assertTrue ("BmmContainerType at path: (\"OBSERVATION\", \"/data/events/data/items\")", Rm102Model.propertyAtPath("OBSERVATION", "/data/events/data/items").getType() instanceof BmmContainerType);
-        assertTrue ("BmmGenericType at path: (\"DV_QUANTITY\", \"/normal_range\")", Rm102Model.propertyAtPath("DV_QUANTITY", "/normal_range").getType() instanceof BmmGenericType);
-        assertTrue ("BmmGSimpleType at path: (\"CARE_ENTRY\", \"/subject\")", Rm102Model.propertyAtPath("CARE_ENTRY", "/subject").getType() instanceof BmmSimpleType);
-    }
-
-    public void testRm102ModelDescendants() throws Exception {
-        // test descendant relations
-        assertTrue ("\"OBSERVATION\" descendant of \"LOCATABLE\")", Rm102Model.descendantOf("OBSERVATION", "LOCATABLE"));
-        assertFalse ("\"LOCATABLE\" not descendant of \"COMPOSITION\")", Rm102Model.descendantOf("LOCATABLE", "COMPOSITION"));
-        assertTrue ("\"ITEM_TREE\" descendant of \"ITEM_STRUCTURE\")", Rm102Model.descendantOf("ITEM_TREE", "ITEM_STRUCTURE"));
-        assertTrue ("\"DV_DURATION\" descendant of \"DV_ORDERED\")", Rm102Model.descendantOf("DV_DURATION", "DV_ORDERED"));
-    }
-
-    public void testRm102CompositionImmediateSuppliers() throws Exception {
-        // test supplier relations
-        Set<String> testResult = Rm102Model.suppliers("COMPOSITION");
-
-        // conformance result from ADL Workbench
-        Set<String> conformanceResult = new LinkedHashSet<>(
-            Arrays.asList(
-                "String", "UID_BASED_ID", "OBJECT_VERSION_ID", "HIER_OBJECT_ID",
-                "DV_TEXT", "DV_CODED_TEXT", "ARCHETYPED", "FEEDER_AUDIT", "LINK", "CODE_PHRASE", "PARTY_PROXY",
-                "PARTY_IDENTIFIED", "PARTY_SELF", "EVENT_CONTEXT", "CONTENT_ITEM", "SECTION", "ENTRY", "GENERIC_ENTRY"
-            )
-        );
-
-        // if anyone wants to see the contents.
-//        System.out.println("Test result: ");
-//        String[] sortedTestResult = testResult.toArray(new String[testResult.size()]);
-//        Arrays.sort (sortedTestResult);
-//        System.out.println(Arrays.toString (sortedTestResult));
-//
-//        System.out.println("Conformance result: ");
-//        String[] sortedConformanceResult = conformanceResult.toArray(new String[conformanceResult.size()]);
-//        Arrays.sort (sortedConformanceResult);
-//        System.out.println(Arrays.toString (sortedConformanceResult));
-
-        assertTrue ("\"COMPOSITION\" has suppliers {\"CODE_PHRASE\", \"DV_TEXT\", ...})", testResult.equals (conformanceResult));
-    }
-
-    public void testRm102PartyIdentifiedSuppliers() throws Exception {
-        // test supplier relations
-        Set<String> testResult = Rm102Model.supplierClosure("PARTY_IDENTIFIED");
-
-        // conformance result from ADL Workbench
-        Set<String> conformanceResult = new LinkedHashSet<>(
-                Arrays.asList(
-                        "String", "PARTY_REF", "DV_IDENTIFIER", "OBJECT_ID",
-                        "TERMINOLOGY_ID", "UID_BASED_ID", "GENERIC_ID", "ARCHETYPE_ID", "TEMPLATE_ID"
-                )
-        );
-
-        // if anyone wants to see the contents.
-//        System.out.println("Test result: ");
-//        String[] sortedTestResult = testResult.toArray(new String[testResult.size()]);
-//        Arrays.sort (sortedTestResult);
-//        System.out.println(Arrays.toString (sortedTestResult));
-//
-//        System.out.println("Conformance result: ");
-//        String[] sortedConformanceResult = conformanceResult.toArray(new String[conformanceResult.size()]);
-//        Arrays.sort (sortedConformanceResult);
-//        System.out.println(Arrays.toString (sortedConformanceResult));
-
-        assertTrue ("\"PARTY_IDENTIFIED\" has suppliers {\"String\", \"PARTY_REF\", ...})", testResult.equals (conformanceResult));
-    }
-
-    private PBmmSchema parse(String name) throws IOException  {
-        try (InputStream stream = getClass().getResourceAsStream(name)) {
-            return BmmOdinParser.convert(stream);
-        }
-    }
 
     @Test
     public void generateOdinTest() throws  Exception{
@@ -204,9 +95,5 @@ public class ConversionTest {
         assertEquals(2, hashContentType.getGenericParameters().size());
         assertEquals("Hash<String,Uri>", hashContentType.toDisplayString());//this is not according to spec perhaps, but it is how the Archie AOM is implemented, and this is a direct conversion
     }
-
-    private BmmModel Rm102Model;
-
-    private BmmModel AdlTestModel;
 
 }
