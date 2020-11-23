5b4235ff74ef1db3b6188855c5f0b45705f75e71
Pieter Bos <pieter.bos@nedap.com>
2020-11-23 17:07:56 +0100

    WIP: DV MULTIMEDIA

---
+++ tools/src/test/java/com/nedap/archie/rmobjectvalidatortest/DvMultimediaInvariantTest.java
@@ -0,0 +1,14 @@
+package com.nedap.archie.rmobjectvalidatortest;
+
+import com.nedap.archie.rm.datavalues.encapsulated.DvEncapsulated;
+import com.nedap.archie.rm.datavalues.encapsulated.DvMultimedia;
+import org.junit.Test;
+
+public class DvMultimediaInvariantTest {
+
+    @Test
+    public void valid() {
+        DvMultimedia dvMultimedia = new DvMultimedia();
+        dvMultimedia.set
+    }
+}
