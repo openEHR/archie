c454702a3404e4c825075b90133fb7dcb1650769
Pieter Bos <pieter.bos@nedap.com>
2020-12-14 13:36:01 +0100

    Fix i18n generation by switching to generating source form instead of .class files directly, and add new strings

--- i18n/compile.sh
+++ i18n/compile.sh
@@ -1,3 +1,3 @@
 # make class files that can be read
-msgfmt --java2 -r openehrArchie -l nl -d ./src/main/resources ./po/i18n_nl.po
-msgfmt --java2 -r openehrArchie -l en -d ./src/main/resources ./po/i18n_en.po
+msgfmt --verbose --java2 --source -r openehrArchie -l nl -d ./src/main/java ./po/i18n_nl.po
+msgfmt --verbose --java2 --source -r openehrArchie -l en -d ./src/main/java ./po/i18n_en.po
