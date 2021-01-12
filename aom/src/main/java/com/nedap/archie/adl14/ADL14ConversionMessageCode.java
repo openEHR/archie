6c5815ab88acf7505f39da268226ff696fc2a428
Pieter Bos <pieter.bos@nedap.com>
2021-01-12 15:27:07 +0100

    Fix typos
    Co-authored-by: Mattijs Kuhlmann <47526389+MattijsK@users.noreply.github.com>

--- aom/src/main/java/com/nedap/archie/adl14/ADL14ConversionMessageCode.java
+++ aom/src/main/java/com/nedap/archie/adl14/ADL14ConversionMessageCode.java
@@ -6,7 +6,7 @@ public enum ADL14ConversionMessageCode implements MessageCode {
     WARNING_SPECIALIZED_FIRST_MATCHING_CHILD("Found two matching children. Only picked one"),
     WARNING_UNKNOWN_CODE_TYPE_IN_TERMBINDING("Termbindings contains at code {0}, but this is unused in the archetype. This cannot be automatically converted"),
     INFO_PREVIOUSLY_CONVERTED_CODE_DELETED("A previously converted path no longer exists. This usually means the source ADL 1.4 has been edited, in that case there's no problem."),
-    INFO_PREVIOUSLY_CONVERTED_CODE_RENAMED("A previously synthesized code is now an explicit code int he ADL 1.4 archetype. The explictly set code will be used instead of the synthesized one. This can cause backwards compatibility issues");
+    INFO_PREVIOUSLY_CONVERTED_CODE_RENAMED("A previously synthesized code is now an explicit code in the ADL 1.4 archetype. The explicitly set code will be used instead of the synthesized one. This can cause backwards compatibility issues");
 
     private String template;
 
