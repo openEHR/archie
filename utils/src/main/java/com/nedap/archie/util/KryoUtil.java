db183bfefdad9c94c473065422d74e3637544f64
EdwardvanRaak <edward.vanraak@nedap.com>
2022-09-29 17:26:59 +0200

    add custom serializers to be added to Kryo instance

--- utils/src/main/java/com/nedap/archie/util/KryoUtil.java
+++ utils/src/main/java/com/nedap/archie/util/KryoUtil.java
@@ -1,8 +1,10 @@
 package com.nedap.archie.util;
 
 import com.esotericsoftware.kryo.kryo5.Kryo;
+import com.esotericsoftware.kryo.kryo5.Serializer;
 import com.esotericsoftware.kryo.kryo5.util.Pool;
 
+import java.util.HashMap;
 
 /**
  * Created by pieter.bos on 03/11/15.
@@ -10,16 +12,23 @@ import com.esotericsoftware.kryo.kryo5.util.Pool;
 public class KryoUtil {
 
     // Build pool with SoftReferences enabled (optional)
-    private static Pool<Kryo> pool;
+    private static final Pool<Kryo> pool;
+
+    @SuppressWarnings("rawtypes")
+    private static final HashMap<Class, Serializer> serializers = new HashMap<>();
 
     static {
         // Pool constructor arguments: thread safe, soft references, maximum capacity
         pool = new Pool<Kryo>(true, false) {
-            protected Kryo create () {
+            protected Kryo create() {
                 Kryo kryo = new Kryo();
+
                 kryo.setRegistrationRequired(false);
                 kryo.setReferences(true);
                 kryo.setCopyReferences(true);
+
+                serializers.forEach(kryo::register);
+
                 return kryo;
             }
         };
@@ -28,5 +37,10 @@ public class KryoUtil {
 
     public static Pool<Kryo> getPool() {
         return pool;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void addSerializer(Class clazz, Serializer serializer) {
+        serializers.put(clazz, serializer);
     }
 }
