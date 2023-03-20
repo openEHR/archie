d0480eaed5ba61abb214aaf31e56fa99ebec1521
Vera Prinsen <vera.prinsen@nedap.com>
2023-03-20 09:26:07 +0100

    Add equals and hashCode to CPrimitiveObject class tree

--- aom/src/main/java/com/nedap/archie/aom/primitives/CReal.java
+++ aom/src/main/java/com/nedap/archie/aom/primitives/CReal.java
@@ -9,6 +9,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * TODO: a real is perhaps not a double.
@@ -46,5 +47,19 @@ public class CReal extends COrdered<Double> {
     @Override
     public void addConstraint(Interval<Double> constraint) {
         this.constraint.add(constraint);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CReal)) return false;
+        if (!super.equals(o)) return false;
+        CReal cReal = (CReal) o;
+        return Objects.equals(assumedValue, cReal.assumedValue) && Objects.equals(constraint, cReal.constraint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), assumedValue, constraint);
     }
 }
