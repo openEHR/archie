0bff2cf2721dfc54a33a80b6a3dfe87c0d425de4
Jelte Zeilstra <jelte.zeilstra@nedap.com>
2023-06-14 14:58:12 +0200

    Simplify SpecificMethodSelector

--- aom/src/main/java/com/nedap/archie/rminfo/SpecificMethodSelector.java
+++ aom/src/main/java/com/nedap/archie/rminfo/SpecificMethodSelector.java
@@ -18,30 +18,14 @@ class SpecificMethodSelector implements BinaryOperator<Method> {
         Class<?> returnType1 = method1.getReturnType();
         Class<?> returnType2 = method2.getReturnType();
 
-        if (class1 == class2) {
-            return mostSpecificReturnType(method1, method2);
+        if (class1 == class2 && returnType1 == returnType2) {
+            throw new IllegalArgumentException("Similar methods " + method1.toGenericString() + " and " + method2.toGenericString());
         } else if (class1.isAssignableFrom(class2) && returnType1.isAssignableFrom(returnType2)) {
             return method2;
         } else if (class2.isAssignableFrom(class1) && returnType2.isAssignableFrom(returnType1)) {
             return method1;
         } else {
             // Methods are incompatible
-            throw new IllegalArgumentException("Incompatible methods " + method1.toGenericString() + " and " + method2.toGenericString());
-        }
-    }
-
-    private Method mostSpecificReturnType(Method method1, Method method2) {
-        Class<?> returnType1 = method1.getReturnType();
-        Class<?> returnType2 = method2.getReturnType();
-
-        if(returnType1 == returnType2) {
-            throw new IllegalArgumentException("Similar methods " + method1.toGenericString() + " and " + method2.toGenericString());
-        } else if (returnType1.isAssignableFrom(returnType2)) {
-            return method2;
-        } else if (returnType2.isAssignableFrom(returnType1)) {
-            return method1;
-        } else {
-            // Return types are incompatible
             throw new IllegalArgumentException("Incompatible methods " + method1.toGenericString() + " and " + method2.toGenericString());
         }
     }
