bfc55e5fbea89371ceba632c9bcce738edca68f5
Mattijs Kuhlmann <mattijs.kuhlmann@nedap.com>
2024-03-07 15:52:46 +0100

    Increase coverage by using same logic as the json version

--- aom/src/main/java/com/nedap/archie/xml/adapters/DateTimeXmlAdapter.java
+++ aom/src/main/java/com/nedap/archie/xml/adapters/DateTimeXmlAdapter.java
@@ -4,9 +4,6 @@ import com.nedap.archie.datetime.DateTimeParsers;
 import com.nedap.archie.datetime.DateTimeSerializerFormatters;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAccessor;
 
@@ -22,14 +19,12 @@ public class DateTimeXmlAdapter extends XmlAdapter<String, TemporalAccessor> {
 
     @Override
     public String marshal(TemporalAccessor value) {
-        if(value instanceof LocalDateTime || value instanceof ZonedDateTime || value instanceof OffsetDateTime) {
-            return value.toString();
-        }
         if(!value.isSupported(ChronoField.HOUR_OF_DAY) &&
                 !value.isSupported(ChronoField.MINUTE_OF_HOUR) &&
                 !value.isSupported(ChronoField.SECOND_OF_MINUTE)) {
             return DateTimeSerializerFormatters.ISO_8601_DATE.format(value);
         }
+
         return DateTimeSerializerFormatters.ISO_8601_DATE_TIME.format(value);
     }
 
