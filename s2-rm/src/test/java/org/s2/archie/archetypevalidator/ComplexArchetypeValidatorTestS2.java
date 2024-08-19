43e2df544fdb8aa2f66d11534c90641baa650650
wolandscat <thomas.beale@graphitehealth.io>
2024-08-19 13:18:12 -0600

    Build out vital_signs-minimal test case further.

--- s2-rm/src/test/java/org/s2/archie/archetypevalidator/ComplexArchetypeValidatorTestS2.java
+++ s2-rm/src/test/java/org/s2/archie/archetypevalidator/ComplexArchetypeValidatorTestS2.java
@@ -18,11 +18,13 @@ import static org.junit.Assert.assertEquals;
 public class ComplexArchetypeValidatorTestS2 {
     private ReferenceModels models;
     private static Archetype archNodeDevice;
-    private static Archetype archNodeCitation;
     private static Archetype archNodeLevelOfExertion;
+    private static Archetype archNodeCitation;
 
+    private static Archetype archNodeWaveform;
     private static Archetype archNodeAdverseReaction;
     private static Archetype archObsBloodPressure;
+    private static Archetype archObsBodyWeight;
     private static Archetype archObsHeartRate;
     private static Archetype archObsHeight;
     private static Archetype archObsBodyTemperature;
@@ -40,32 +42,36 @@ public class ComplexArchetypeValidatorTestS2 {
 
         archNodeDevice = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Node.device.v1.1.2.adls"));
         archNodeAdverseReaction = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Node.adverse_reaction_event.v1.0.0.adls"));
-        archObsBloodPressure = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.blood_pressure.v2.0.8.adls"));
-        archObsHeartRate = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.heart_rate.v1.0.0.adls"));
         archNodeCitation = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Node.citation.v0.0.1-alpha.adls"));
         archNodeLevelOfExertion = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Node.level_of_exertion.v0.0.1-alpha.adls"));
+        archNodeWaveform = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Node.waveform.v0.0.1-alpha.adls"));
 
         archObsHeight = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.height.v2.1.0.adls"));
+        archObsBodyWeight = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.body_weight.v2.1.6.adls"));
         archObsBodyTemperature = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.body_temperature.v2.1.5.adls"));
         archObsRespiratoryRate = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.respiratory_rate.v1.0.0.adls"));
         archObsBmi = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.body_mass_index.v2.0.6.adls"));
+        archObsBloodPressure = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.blood_pressure.v2.0.8.adls"));
+        archObsHeartRate = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Direct_observation.heart_rate.v1.0.0.adls"));
 
         archCompEncounter = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Composition.encounter.v1.0.0.adls"));
         tplCompVitalSigns = new ADLParser().parse(ComplexArchetypeValidatorTestS2.class.getResourceAsStream("s2-EHR-Composition.t_encounter-vital_signs-minimal.v1.0.0.adls"));
 
         repository = new InMemoryFullArchetypeRepository();
 
-        repository.addArchetype(archObsHeight);
-        repository.addArchetype(archObsBodyTemperature);
-        repository.addArchetype(archObsRespiratoryRate);
-        repository.addArchetype(archObsBmi);
-
         repository.addArchetype(archNodeCitation);
         repository.addArchetype(archNodeLevelOfExertion);
         repository.addArchetype(archNodeDevice);
         repository.addArchetype(archNodeAdverseReaction);
+        repository.addArchetype(archNodeWaveform);
+
         repository.addArchetype(archObsBloodPressure);
         repository.addArchetype(archObsHeartRate);
+        repository.addArchetype(archObsHeight);
+        repository.addArchetype(archObsBodyWeight);
+        repository.addArchetype(archObsBodyTemperature);
+        repository.addArchetype(archObsRespiratoryRate);
+        repository.addArchetype(archObsBmi);
 
         repository.addArchetype(archCompEncounter);
         repository.addArchetype(tplCompVitalSigns);
