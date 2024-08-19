d7d04dc423bf09163a22e4d462a26af4ac60c549
wolandscat <thomas.beale@graphitehealth.io>
2024-08-19 13:17:30 -0600

    Missed additions of inheritance to RMObject

--- s2-rm/src/main/java/org/s2/rm/base/model_support/definitions/ValidityKind.java
+++ s2-rm/src/main/java/org/s2/rm/base/model_support/definitions/ValidityKind.java
@@ -1,5 +1,7 @@
 package org.s2.rm.base.model_support.definitions;
 
+import com.nedap.archie.base.RMObject;
+
 import java.util.*;
 import javax.xml.bind.annotation.*;
 
@@ -10,7 +12,7 @@ import javax.xml.bind.annotation.*;
 */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Validity_kind", propOrder = {"value"})
-public class ValidityKind {
+public class ValidityKind extends RMObject {
   /**
   * Enumeration value.
   */
