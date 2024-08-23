f741011bae3a63dbf6ce7cf5736690f5afaa8690
wolandscat <thomas.beale@graphitehealth.io>
2024-08-23 16:19:09 -0600

    Make simple CodedText Object -> JSON test work

--- s2-rm/src/test/java/org/s2/rm/base/data_types/text/CodedTextTest.java
+++ s2-rm/src/test/java/org/s2/rm/base/data_types/text/CodedTextTest.java
@@ -1,14 +1,21 @@
 package org.s2.rm.base.data_types.text;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.nedap.archie.rminfo.ReferenceModels;
+import org.junit.Before;
 import org.junit.Test;
 import org.s2.rm.base.foundation_types.terminology.TerminologyCode;
 import org.s2.rm.base.foundation_types.terminology.TerminologyTerm;
+import org.s2.rminfo.S2RmInfoLookup;
+import org.s2.serialisation.json.S2RmJacksonUtil;
+import org.s2.serialisation.json.S2RmObjectMapperProvider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class CodedTextTest {
-
     @Test
     public void testEquals() {
         CodedText codedTextOne = new CodedText(new TerminologyTerm("icd10::123 preferred term", new TerminologyCode("icd10", "123")), "some screen text");
@@ -20,13 +27,24 @@ public class CodedTextTest {
     }
 
     @Test
-    public void testToString() {
+    public void testToString() throws JsonProcessingException {
+        ObjectMapper om = S2RmJacksonUtil.getObjectMapper();
+
         CodedText codedTextOne = new CodedText();
+        String codedTextOneJson = om.writeValueAsString(codedTextOne);
+        assertEquals("{ }", codedTextOneJson);
+
         CodedText codedTextTwo = new CodedText(new TerminologyTerm("icd10::123 preferred term", new TerminologyCode("icd10", "123")), "some screen text");
-
-        assertEquals("CodedText{term=null, text=null}", codedTextOne.toString());
-
-        // TODO: dont know what the output of toString should be - possibly standard JSON? So fix the below when we know.
-        assertEquals("CodedText{term=TerminologyTerm{description=\"icd10::123 preferred term\", concept=TerminologyTerm{terminologyId=\"icd10\", codeString=\"123\"}}, text=\"some screen text\"}", codedTextTwo.toString());
+        String codedTextTwoJson = om.writeValueAsString(codedTextTwo);
+        assertEquals("{\n" +
+                "  \"text\" : \"some screen text\",\n" +
+                "  \"term\" : {\n" +
+                "    \"description\" : \"icd10::123 preferred term\",\n" +
+                "    \"concept\" : {\n" +
+                "      \"terminology_id\" : \"icd10\",\n" +
+                "      \"code_string\" : \"123\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}", codedTextTwoJson);
     }
 }
