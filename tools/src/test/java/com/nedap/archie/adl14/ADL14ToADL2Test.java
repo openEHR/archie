9f915a398150068b16ae360247894f01126deb60
Vera Prinsen <vera.prinsen@nedap.com>
2026-01-26 09:59:08 +0100

    Update tests

--- tools/src/test/java/com/nedap/archie/adl14/ADL14ToADL2Test.java
+++ tools/src/test/java/com/nedap/archie/adl14/ADL14ToADL2Test.java
@@ -13,9 +13,14 @@ import org.openehr.referencemodels.BuiltinReferenceModels;
 
 import java.io.InputStream;
 import java.net.URI;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
