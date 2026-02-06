3e3d8834a318097d274c89932cab111ba775904f
Jelte Zeilstra <jelte.zeilstra@nedap.com>
2026-02-06 10:09:38 +0100

    Change DuplicateKeyException to extend RuntimeException

--- tools/src/main/java/com/nedap/archie/json/flat/DuplicateKeyException.java
+++ tools/src/main/java/com/nedap/archie/json/flat/DuplicateKeyException.java
@@ -4,7 +4,7 @@ package com.nedap.archie.json.flat;
  * Exception to indicate a key in the flat json has been added twice, and that this RM Object can thus not be
  * properly converted to flat json. This should not happen with proper input.
  */
-public class DuplicateKeyException extends Exception {
+public class DuplicateKeyException extends RuntimeException {
 
     public DuplicateKeyException(String message) {
         super(message);
