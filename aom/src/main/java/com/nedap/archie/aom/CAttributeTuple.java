b9c7939fd337d79da01099b56710211a9bb670bf
Vera Prinsen <vera.prinsen@nedap.com>
2026-03-16 09:53:21 +0100

    Requested Changes

--- aom/src/main/java/com/nedap/archie/aom/CAttributeTuple.java
+++ aom/src/main/java/com/nedap/archie/aom/CAttributeTuple.java
@@ -3,10 +3,9 @@ package com.nedap.archie.aom;
 
 import com.nedap.archie.rminfo.AttributeAccessor;
 import com.nedap.archie.rminfo.ModelInfoLookup;
+import jakarta.xml.bind.annotation.XmlType;
 
 import javax.annotation.Nullable;
-import jakarta.xml.bind.annotation.XmlType;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
