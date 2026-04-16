1cbf140e7a233ec01dff35e14a60933f4bcf465c
Vera Prinsen <vera.prinsen@nedap.com>
2026-04-16 11:22:59 +0200

    Fix test now that the order of terminology codes is normal again

--- tools/src/test/java/com/nedap/archie/adl14/ADL14ToADL2Test.java
+++ tools/src/test/java/com/nedap/archie/adl14/ADL14ToADL2Test.java
@@ -1,24 +1,16 @@
 package com.nedap.archie.adl14;
 
-import com.nedap.archie.adlparser.ADLParser;
 import com.nedap.archie.aom.Archetype;
-import com.nedap.archie.aom.CAttribute;
-import com.nedap.archie.aom.CObject;
-import com.nedap.archie.aom.terminology.ArchetypeTerm;
-import com.nedap.archie.aom.terminology.ArchetypeTerminology;
-import com.nedap.archie.aom.terminology.ValueSet;
-import com.nedap.archie.rminfo.ArchieAOMInfoLookup;
+import com.nedap.archie.serializer.adl.ADLArchetypeSerializer;
+import org.antlr.v4.runtime.CharStreams;
+import org.apache.commons.io.input.BOMInputStream;
 import org.junit.jupiter.api.Test;
 import org.openehr.referencemodels.BuiltinReferenceModels;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.util.Arrays;
-import java.util.Map;
-import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class ADL14ToADL2Test {
@@ -32,11 +24,6 @@ public class ADL14ToADL2Test {
             ADL14Parser parser = new ADL14Parser(BuiltinReferenceModels.getMetaModels());
             adl14 = parser.parse(stream, ConversionConfigForTest.getConfig());
         }
-        Archetype expected;
-        try (InputStream stream = getClass().getResourceAsStream(path + "openEHR-EHR-OBSERVATION.demo_adl2_id.v1.0.0.adls")) {
-            ADLParser parser = new ADLParser(BuiltinReferenceModels.getMetaModels());
-            expected = parser.parse(stream);
-        }
 
         // Configuration defaults to ID_CODED
         ADL14Converter converter = new ADL14Converter(BuiltinReferenceModels.getMetaModels(), new ADL14ConversionConfiguration());
@@ -44,9 +31,10 @@ public class ADL14ToADL2Test {
         ADL2ConversionResultList resultList = converter.convert(Arrays.asList(adl14));
         Archetype result = resultList.getConversionResults().get(0).getArchetype();
 
-        assertEquals(ArchieAOMInfoLookup.ADL_VERSION, result.getAdlVersion());
-        assertArchetypes(expected, result);
-        assertArchetypes(result, expected); // Also check the reverse, to check if the result doesn't have more elements than the expected archetype
+        assertEquals(
+                CharStreams.fromStream(new BOMInputStream(getClass().getResourceAsStream(path + "openEHR-EHR-OBSERVATION.demo_adl2_id.v1.0.0.adls"))).toString(),
+                ADLArchetypeSerializer.serialize(result)
+        );
     }
 
     @Test
@@ -56,11 +44,6 @@ public class ADL14ToADL2Test {
             ADL14Parser parser = new ADL14Parser(BuiltinReferenceModels.getMetaModels());
             adl14 = parser.parse(stream, ConversionConfigForTest.getConfig());
         }
-        Archetype expected;
-        try (InputStream stream = getClass().getResourceAsStream(path + "openEHR-EHR-OBSERVATION.demo_adl2_at.v1.0.0.adls")) {
-            ADLParser parser = new ADLParser(BuiltinReferenceModels.getMetaModels());
-            expected = parser.parse(stream);
-        }
 
         ADL14ConversionConfiguration configuration = new ADL14ConversionConfiguration();
         configuration.setNodeIdCodeSystem(ADL14ConversionConfiguration.NodeIdCodeSystem.AT_CODED);
@@ -69,9 +52,10 @@ public class ADL14ToADL2Test {
         ADL2ConversionResultList resultList = converter.convert(Arrays.asList(adl14));
         Archetype result = resultList.getConversionResults().get(0).getArchetype();
 
-        assertEquals(ArchieAOMInfoLookup.ADL_VERSION, result.getAdlVersion());
-        assertArchetypes(expected, result);
-        assertArchetypes(result, expected); // Also check the reverse, to check if the result doesn't have more elements than the expected archetype
+        assertEquals(
+                CharStreams.fromStream(new BOMInputStream(getClass().getResourceAsStream(path + "openEHR-EHR-OBSERVATION.demo_adl2_at.v1.0.0.adls"))).toString(),
+                ADLArchetypeSerializer.serialize(result)
+        );
     }
 
     @Test
@@ -86,16 +70,6 @@ public class ADL14ToADL2Test {
             ADL14Parser parser = new ADL14Parser(BuiltinReferenceModels.getMetaModels());
             child = parser.parse(stream, ConversionConfigForTest.getConfig());
         }
-        Archetype parentExpected;
-        try (InputStream stream = getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam_adl2_id.v2.1.3.adls")) {
-            ADLParser parser = new ADLParser(BuiltinReferenceModels.getMetaModels());
-            parentExpected = parser.parse(stream);
-        }
-        Archetype childExpected;
-        try (InputStream stream = getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam-abdomen_adl2_id.v0.0.1-alpha.adls")) {
-            ADLParser parser = new ADLParser(BuiltinReferenceModels.getMetaModels());
-            childExpected = parser.parse(stream);
-        }
 
         // Configuration defaults to ID_CODED
         ADL14Converter converter = new ADL14Converter(BuiltinReferenceModels.getMetaModels(), new ADL14ConversionConfiguration());
@@ -104,13 +78,14 @@ public class ADL14ToADL2Test {
         Archetype parentResult = resultList.getConversionResults().get(0).getArchetype();
         Archetype childResult = resultList.getConversionResults().get(1).getArchetype();
 
-        assertEquals(ArchieAOMInfoLookup.ADL_VERSION, parentResult.getAdlVersion());
-        assertArchetypes(parentExpected, parentResult);
-        assertArchetypes(parentResult, parentExpected); // Also check the reverse, to check if the result doesn't have more elements than the expected archetype
-
-        assertEquals(ArchieAOMInfoLookup.ADL_VERSION, childResult.getAdlVersion());
-        assertArchetypes(childExpected, childResult);
-        assertArchetypes(childResult, childExpected); // Also check the reverse, to check if the result doesn't have more elements than the expected archetype
+        assertEquals(
+                CharStreams.fromStream(new BOMInputStream(getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam_adl2_id.v2.1.3.adls"))).toString(),
+                ADLArchetypeSerializer.serialize(parentResult)
+        );
+        assertEquals(
+                CharStreams.fromStream(new BOMInputStream(getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam-abdomen_adl2_id.v0.0.1-alpha.adls"))).toString(),
+                ADLArchetypeSerializer.serialize(childResult)
+        );
     }
 
     @Test
@@ -125,16 +100,6 @@ public class ADL14ToADL2Test {
             ADL14Parser parser = new ADL14Parser(BuiltinReferenceModels.getMetaModels());
             child = parser.parse(stream, ConversionConfigForTest.getConfig());
         }
-        Archetype parentExpected;
-        try (InputStream stream = getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam_adl2_at.v2.1.3.adls")) {
-            ADLParser parser = new ADLParser(BuiltinReferenceModels.getMetaModels());
-            parentExpected = parser.parse(stream);
-        }
-        Archetype childExpected;
-        try (InputStream stream = getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam-abdomen_adl2_at.v0.0.1-alpha.adls")) {
-            ADLParser parser = new ADLParser(BuiltinReferenceModels.getMetaModels());
-            childExpected = parser.parse(stream);
-        }
         
         ADL14ConversionConfiguration configuration = new ADL14ConversionConfiguration();
         configuration.setNodeIdCodeSystem(ADL14ConversionConfiguration.NodeIdCodeSystem.AT_CODED);
@@ -144,63 +109,13 @@ public class ADL14ToADL2Test {
         Archetype parentResult = resultList.getConversionResults().get(0).getArchetype();
         Archetype childResult = resultList.getConversionResults().get(1).getArchetype();
 
-        assertEquals(ArchieAOMInfoLookup.ADL_VERSION, parentResult.getAdlVersion());
-        assertArchetypes(parentExpected, parentResult);
-        assertArchetypes(parentResult, parentExpected); // Also check the reverse, to check if the result doesn't have more elements than the expected archetype
-
-        assertEquals(ArchieAOMInfoLookup.ADL_VERSION, childResult.getAdlVersion());
-        assertArchetypes(childExpected, childResult);
-        assertArchetypes(childResult, childExpected); // Also check the reverse, to check if the result doesn't have more elements than the expected archetype
-    }
-
-    private void assertArchetypes(Archetype expected, Archetype actual) {
-        ArchetypeTerminology expectedTerminology = expected.getTerminology();
-        ArchetypeTerminology actualTerminology = actual.getTerminology();
-
-        // Definition
-        Stack<CObject> stack = new Stack<>();
-        stack.push(expected.getDefinition());
-        while (!stack.isEmpty()) {
-            CObject object = stack.pop();
-            assertNotNull(actual.itemAtPath(object.getPath()));
-            for (CAttribute attribute : object.getAttributes()) {
-                for (CObject cObject : attribute.getChildren()) {
-                    stack.push(cObject);
-                }
-            }
-        }
-
-        // TermDefinitions
-        Map<String, Map<String, ArchetypeTerm>> expectedTermDefinitions = expectedTerminology.getTermDefinitions();
-        Map<String, Map<String, ArchetypeTerm>> actualTermDefinitions = actualTerminology.getTermDefinitions();
-
-        assertEquals(expectedTermDefinitions.keySet(), actualTermDefinitions.keySet());
-        expectedTermDefinitions.keySet().forEach(key -> {
-            assertEquals(expectedTermDefinitions.get(key).keySet(), actualTermDefinitions.get(key).keySet());
-            expectedTermDefinitions.get(key).keySet().forEach(key2 -> {
-                assertEquals(expectedTermDefinitions.get(key).get(key2), expectedTermDefinitions.get(key).get(key2));
-            });
-        });
-
-        // TermBindings
-        Map<String, Map<String, URI>> expectedTermBindings = expectedTerminology.getTermBindings();
-        Map<String, Map<String, URI>> actualTermBindings = actualTerminology.getTermBindings();
-
-        assertEquals(expectedTermBindings.keySet(), actualTermBindings.keySet());
-        expectedTermBindings.keySet().forEach(key -> {
-            assertEquals(expectedTermBindings.get(key).keySet(), actualTermBindings.get(key).keySet());
-            expectedTermBindings.get(key).keySet().forEach(key2 -> {
-                assertEquals(expectedTermBindings.get(key).get(key2), actualTermBindings.get(key).get(key2));
-            });
-        });
-
-        // ValueSets
-        Map<String, ValueSet> expectedValueSets = expectedTerminology.getValueSets();
-        Map<String, ValueSet> actualValueSets = actualTerminology.getValueSets();
-
-        assertEquals(expectedValueSets.keySet(), actualValueSets.keySet());
-        expectedValueSets.keySet().forEach(key -> {
-            assertEquals(expectedValueSets.get(key), actualValueSets.get(key));
-        });
+        assertEquals(
+                CharStreams.fromStream(new BOMInputStream(getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam_adl2_at.v2.1.3.adls"))).toString(),
+                ADLArchetypeSerializer.serialize(parentResult)
+        );
+        assertEquals(
+                CharStreams.fromStream(new BOMInputStream(getClass().getResourceAsStream(path + "openEHR-EHR-CLUSTER.exam-abdomen_adl2_at.v0.0.1-alpha.adls"))).toString(),
+                ADLArchetypeSerializer.serialize(childResult)
+        );
     }
 }
